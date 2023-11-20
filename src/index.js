4518aeae68b707a81f2c7cb141078f87feb6dbc9
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-21 00:29:37 +0200

    resp

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_MCbmjbpL2EK0M1ExHHJ0clpK8ztrjpAtYLeQ5PmMGUjuQ3wzfscrLeVh8obG0Lbz';
+const BASE_URL = `https://api.thecatapi.com/v1/breeds`;
+
+
+
+axios
+  .get(BASE_URL)
+  .then(response => {
+    console.log('Cat breeds:', response.data);
+  })
+  .catch(error => {
+    console.error('Error fetching cat breeds:', error);
+  });
