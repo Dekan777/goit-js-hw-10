62f5e082c6e709e70495a96da76268a6a2097619
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-21 12:55:07 +0200

    fetchCatByBreed

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,6 +5,7 @@ axios.defaults.headers.common['x-api-key'] =
   'live_MCbmjbpL2EK0M1ExHHJ0clpK8ztrjpAtYLeQ5PmMGUjuQ3wzfscrLeVh8obG0Lbz';
 
 const BASE_URL = `https://api.thecatapi.com/v1/breeds`;
+const BASE_URL_INFO = `https://api.thecatapi.com/v1/images/search`;
 
 export function fetchBreeds() {
   return axios
@@ -15,3 +16,15 @@ export function fetchBreeds() {
       throw error;
     });
 }
+
+
+ export function fetchCatByBreed(breedId) {
+    const catInfoURL = `${BASE_URL_INFO}?breed_ids=${breedId}`;
+  
+    return axios.get(catInfoURL)
+      .then(response => response.data[0])
+      .catch(error => {
+        console.error('Error fetching cat information:', error);
+        throw error;
+      });
+  }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import axios from 'axios';
 import { fetchBreeds } from './cat-api.js';
-axios.defaults.headers.common['x-api-key'] =
-  'live_MCbmjbpL2EK0M1ExHHJ0clpK8ztrjpAtYLeQ5PmMGUjuQ3wzfscrLeVh8obG0Lbz';
-const BASE_URL = `https://api.thecatapi.com/v1/breeds`;
+import { fetchCatByBreed } from './cat-api.js';
+
+const catInfoContainer = document.querySelector('.cat-info');
 
 function populateBreedsSelect(breeds) {
-  const selectElement = document.querySelector('.breed-select');
-
   // Очищаем существующие опции, если они есть
   selectElement.innerHTML = '';
 
@@ -17,12 +14,11 @@ function populateBreedsSelect(breeds) {
     option.text = breed.name; // Название породы в качестве текста
     // console.log(option.value);
     return option;
-    
   });
 
   // Добавляем все опции в select одним блоком
   selectElement.append(...breedOptions);
-//   console.log(breedOptions);
+  //   console.log(breedOptions);
 }
 
 fetchBreeds()
@@ -33,3 +29,43 @@ fetchBreeds()
   .catch(error => {
     console.error('Error fetching cat breeds:', error);
   });
+
+const selectElement = document.querySelector('.breed-select');
+
+selectElement.addEventListener('change', async event => {
+  const selectedBreedId = event.target.value;
+  fetchCatByBreed(selectedBreedId)
+    .then(catInfo => {
+      console.log('Cat information:', catInfo);
+      // Здесь вы можете использовать информацию о коте по вашему усмотрению
+    })
+    .catch(error => {
+      // Обработка ошибок, если необходимо
+    });
+});
+
+selectElement.addEventListener('change', async event => {
+  const selectedBreedId = event.target.value;
+  fetchCatByBreed(selectedBreedId)
+    .then(catInfo => {
+      // Очищаем существующую информацию о коте
+      catInfoContainer.innerHTML = '';
+
+      // Создаем изображение и блок с информацией о коте
+      const catImage = document.createElement('img');
+      catImage.src = catInfo.url;
+      catImage.alt = 'Cat Image';
+      catImage.style.width = '300px'; // Устанавливаем ширину изображения
+      catImage.style.height = 'auto'; // Сохраняем пропорции
+      catInfoContainer.appendChild(catImage);
+
+      const catInfoDetails = document.createElement('div');
+      catInfoDetails.innerHTML = `<p><strong>Name:</strong> ${catInfo.breeds[0].name}</p>
+                                <p><strong>Description:</strong> ${catInfo.breeds[0].description}</p>
+                                <p><strong>Temperament:</strong> ${catInfo.breeds[0].temperament}</p>`;
+      catInfoContainer.appendChild(catInfoDetails);
+    })
+    .catch(error => {
+      console.error('Error fetching cat information:', error);
+    });
+});
